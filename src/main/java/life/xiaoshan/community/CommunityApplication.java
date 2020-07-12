ebc2a1cfc4af6a59dc61960f347b1320501e167f
24kGarry <39556393+24kGarry@users.noreply.github.com>
2020-07-12 21:29:02 +0800

    init repo

---
+++ src/main/java/life/xiaoshan/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package life.xiaoshan.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
